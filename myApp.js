352a0a9f16dbbbe5c3865b943cbb7c327c65388c
Glitch (tabby-comet-dove) <none>
2022-02-20 15:07:04 +0000

    🛸🌞 Updated with Glitch

---
+++ myApp.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var app = express();
+
+
+
+// midddleware
+app.use((req,res,next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
+// API's 
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname +"/views/index.html")
+})
+
+app.get("/json",(req,res) => {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"})
+  }
+  else res.json({"message": "Hello json"})
+})
+ 
+
+app.get("/now", (req,res,next)=>{
+  req.time = new Date().toString();
+  next();
+},(req,res) => {
+  res.json({"time" : req.time})
+});
+
+
+
+app.get("/:word/echo",(req,res) => {
+ res.json({"echo": req.params.word})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
